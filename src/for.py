f06bfc3b333187a93b6544cdc7906530d6ca3e8b
Adriano A Costa <sdcomputadores@gmail.com>
2017-01-13 15:38:07 -0200

    for

---
+++ src/for.py
@@ -0,0 +1,18 @@
+lista = [1, 2, 3, 5, 8, 13, 21, 34, 56, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946]
+
+#for i in lista:
+#    print(i)
+
+#for i in lista[3:10]:
+#    print(i)
+
+
+#for i in lista[15:]:
+#    if i > 5000:
+#        print(i)
+        
+novalista = ["Adriano", "Araujo", "Costa"]
+
+for j in novalista:
+    if not j == "Adriano":
+        print(j)
