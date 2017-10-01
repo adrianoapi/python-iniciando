715daacf874027cba5cd6db0b57955949dba1da5
Adriano A Costa <sdcomputadores@gmail.com>
2017-10-01 13:27:53 -0300

    update urllib2

--- src/use_urllib2.py
+++ src/use_urllib2.py
@@ -2,6 +2,37 @@
 # To change this template file, choose Tools | Templates
 # and open the template in the editor.
 
-import requests
-r = requests.get('https://github.com/timeline.json')
-r.json()
+import urllib2
+response = urllib2.urlopen('http://python.org/')
+print "Response:", response
+
+# Pega a real URL 
+print "The URL is: ", response.geturl()
+
+# Pega o codigo
+print "Esse eh o codigo: ", response.code
+
+# Pega os cabecalhos
+# This returns a dictionary-like object that describes the page fetched, 
+# particularly the headers sent by the server
+print "The Headers are: ", response.info()
+
+# Get the date part of the header
+print "The Date is: ", response.info()['date']
+
+# Get the server part of the header
+print "The Server is: ", response.info()['server']
+
+# Get all data
+html = response.read()
+print "Get all data: ", html
+
+# Get only the length
+print "Get the length :", len(html)
+
+# Showing that the file object is iterable
+for line in response:
+ print line.rstrip()
+
+# Note that the rstrip strips the trailing newlines and carriage returns before
+# printing the output.
